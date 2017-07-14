cd7c86cbc4dd85398c10f89dc6f0f2299f4ff06d
Josh Gum <revgum@gmail.com>
2017-07-14 11:25:37 -0700

    adds admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713213045) do
+ActiveRecord::Schema.define(version: 20170714182407) do
 
   create_table "expense_types", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170713213045) do
     t.string "username"
     t.integer "pidm"
     t.boolean "certifier", default: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
