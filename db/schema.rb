9ccbf3cccd24bf4fac49e165883281a69c83b3cd
Josh Gum <revgum@gmail.com>
2017-07-14 11:20:27 -0700

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170713213045) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean "admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
