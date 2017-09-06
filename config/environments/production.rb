cb53ca4511dc8f180a9a09ea39959bd57f452ca2
Josh Gum <revgum@gmail.com>
2017-09-06 11:21:57 -0700

    updates to use local_env.yml for server host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tresbien_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => ENV['SERVER_HOST'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
