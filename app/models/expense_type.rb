46b35972c32f92ecc0a411dc2de04f6532fae18a
Josh Gum <revgum@gmail.com>
2017-09-13 09:18:56 -0700

    moves above per diem to the model scope

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -1,5 +1,6 @@
 class ExpenseType < ApplicationRecord
   scope :active, -> { where(active:true) }
+  scope :above_per_diem, -> { where(name:'Above Per Diem') }
   validates :name, presence:true
   has_many :expense_others
 end
