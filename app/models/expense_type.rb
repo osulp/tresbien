1377854e96be6238af6705722de6722cd93ba16e
Josh Gum <revgum@gmail.com>
2017-10-11 07:52:53 -0700

    adds missing functionality froms and administration

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -1,6 +1,7 @@
 class ExpenseType < ApplicationRecord
-  scope :active, -> { where(active:true) }
-  scope :above_per_diem, -> { where(name:'Above Per Diem') }
+  scope :active, -> { where(active: true) }
+  scope :above_per_diem, -> { where(name: 'Above Per Diem') }
+  scope :not_above_per_diem, -> { where.not(name: 'Above Per Diem') }
   validates :name, presence:true
   has_many :expense_others
 end
