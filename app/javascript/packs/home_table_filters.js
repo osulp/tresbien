ddafabce0789053817412f216b7e4a553d8c91e5
Fischer Jemison <jemisonf@oregonstate.edu>
2017-10-12 14:26:45 -0700

    Fixes table filters

--- app/javascript/packs/home_table_filters.js
+++ app/javascript/packs/home_table_filters.js
@@ -1,25 +1,40 @@
 // return true if array2 is a subset of array1. Both must be Javascript arrays. Ignores 'hidden' class in second array
 isSubset = function(array1, array2) {
-  for (var i = 0; i < array2.length; i++) {
-    if (!array1.includes(array2[i]) && array2[i] != 'hidden' ) return false
-  } 
-  return true;
+  return !(array2.filter((e) => e !== 'hidden')
+    .some((e, i, a) => !array1.includes(e)))
 }
 // when a filter checkbox is checked or unchecked, hide/show each row of reimbursement requests that doesn't/does correspond to the set of checked filters
-$(document).on('change', '.filter', function(e) {
-  classes = [];
-  checked_boxes = $('.filter').map(function() { if ($(this).is(':checked')) return $(this).attr('data-filter-target') }).toArray();
-  checkbox = $(this);
-  targetClass = $(this).attr("data-filter-target");
-  parentRow = checkbox.parents(".card-body").parents(".row");
-  parentRow.next().find("tr").each(function() {
-    if ($(this).attr('class') && $(this).attr('class').includes(targetClass)) {
-      classes = $(this).attr('class').split(' '); 
-      if (checkbox.is(':checked') && isSubset(checked_boxes, classes)) {
-        $(this).removeClass('hidden');
-      } else if (!isSubset(checked_boxes, classes)) {
-        $(this).addClass('hidden');
-      }
-    }
+$(document)
+  .on('change', '.filter', function(e) {
+    let checked_boxes = $('.filter')
+      .map(function() {
+        if ($(this)
+          .is(':checked')) return $(this)
+          .attr('data-filter-target')
+      })
+      .toArray();
+    let checkbox = $(this);
+    let targetClass = $(this)
+      .attr("data-filter-target");
+    let parentRow = checkbox.parents(".card-body")
+      .parents(".row");
+    parentRow.next()
+      .find("tr")
+      .each(function() {
+        if ($(this)
+          .attr('class') && $(this)
+          .attr('class')
+          .includes(targetClass)) {
+          let classes = $(this)
+            .attr('class')
+            .split(' ');
+          if (checkbox.is(':checked') && isSubset(checked_boxes, classes)) {
+            $(this)
+              .removeClass('hidden');
+          } else if (!isSubset(checked_boxes, classes)) {
+            $(this)
+              .addClass('hidden');
+          }
+        }
+      });
   });
-}); 
